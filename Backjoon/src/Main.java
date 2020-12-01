308a09fc57b5eda57b5d9fe222a04d97601f4967
jungheun9 <jungheun99@naver.com>
2020-12-02 01:00:56 +0900

    섬의 개수 (graph, grouping)

--- Backjoon/src/Main.java
+++ Backjoon/src/Main.java
@@ -1,92 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
-
-   public static void main(String[] args) throws IOException {
-      
-      BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
-      StringBuilder results = new StringBuilder();
-      
-      int nCount = Integer.parseInt(scan.readLine());
-      
-      Node[] nodes = new Node[nCount+1];
-      for(int i = 1; i < nCount+1; i++) {
-    	  nodes[i] = new Node(i);
-      }
-      
-      for(int i = 0; i < nCount-1; i++) {
-    	  String[] parsedStr = scan.readLine().split(" ");
-    	  int num1 = Integer.parseInt(parsedStr[0]);
-    	  int num2 = Integer.parseInt(parsedStr[1]);
-    	  
-    	  nodes[num1].connect(nodes[num2]);
-    	  nodes[num2].connect(nodes[num1]);
-      }
-      
-      int[] parentOfNode = new int[nCount+1];
-      boolean[] isVisited = new boolean[nCount+1];
-      
-      // root node부터 찾기 시작
-      findAllParentOfNode(parentOfNode, isVisited, nodes[1]); 
-      
-      for(int i = 2; i < nCount+1; i++) {
-    	  results.append(parentOfNode[i] + "\n");
-      }
-      
-      System.out.print(results);
-      
-      scan.close();
-   }
-   
-   private static void findAllParentOfNode(int[] parentOfNode, boolean[] isVisited, Node root) {
-	   
-	   LinkedList<Node> queue = new LinkedList<>();
-	   isVisited[root.num] = true;
-	   queue.add(root);
-	   
-	   while(!queue.isEmpty()) {
-		   int size = queue.size();
-		   for(int i = 0; i < size; i++) {
-			   Node parent = queue.pop();   
-			   for(int j = 0; j < parent.connects.size(); j++) {
-				   Node child = parent.connects.get(j);
-				   if(!isVisited[child.num]) {
-					   isVisited[child.num] = true;
-					   parentOfNode[child.num] = parent.num; 
-					   queue.add(child);
-				   }
-			   }
-		   }
-	   }
-   }
-}
-
-class Node {
-	List<Node> connects;
-	int num;
 	
-	public Node(int n) {
-		this.connects = new ArrayList<>();
-		this.num = n;
+	final static int SEA = 0;
+	final static int LAND = 1;
+	
+	// 0시 부터 시계방향으로(위, 오른쪽 위, 오른쪽, ...)
+	final static int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+	final static int[] dy = {1, 1, 0, -1, -1, -1, 0, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		
+		do {
+			String[] conditions = scan.readLine().split(" ");
+			int width = Integer.parseInt(conditions[0]);
+			int height = Integer.parseInt(conditions[1]);
+			if(width == 0 && height == 0) break;
+			
+			int[][] map = new int[height][width];
+			makeMap(scan, map, width, height);
+			
+			int numOfGroup = findIsland(map, width, height);
+			
+			result.append(numOfGroup + "\n");
+		} while(true);
+		
+		System.out.print(result);
+		
+		scan.close();
 	}
 	
-	public void connect(Node other) {
-		connects.add(other);
+	private static void makeMap(BufferedReader scan, int[][] map, int width, int height) throws IOException {
+		for(int i = 0; i < height; i++) {
+			String[] line = scan.readLine().split(" ");
+			for(int j = 0; j < width; j++) {
+				map[i][j] = Integer.parseInt(line[j]);
+			}
+		}
 	}
 	
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Node " + num + " : ");
-		for(int i = 0; i < connects.size(); i++) {
-			sb.append(connects.get(i).num + ", ");
+	private static int findIsland(int[][] map, int width, int height) {
+		
+		int numOfIsland = 0;
+		for(int x = 0; x < height; x++) {
+			for(int y = 0; y < width; y++) {
+				if(map[x][y] == LAND) {
+					numOfIsland++;
+					findNearIsland(map, width, height, x, y);
+				}
+			}
+		}
+		return numOfIsland;
+	}
+	
+	private static void findNearIsland(int[][] map, int width, int height, int xIndex, int yIndex) {
+		map[xIndex][yIndex] = SEA;
+		
+		for(int i = 0; i < 8; i++) {
+			int x = xIndex + dx[i];
+			int y = yIndex + dy[i];
+			
+			if(0 <= x && x < height && 0 <= y && y < width) {
+				if(map[x][y] == LAND) findNearIsland(map, width, height, x, y);
+			}
 		}
 		
-		return sb.toString();
+		return;
 	}
 }

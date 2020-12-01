ddf7b7564e2a08d45e602a2f0d1ba22afee6d416
jungheun9 <jungheun99@naver.com>
2020-12-02 00:31:45 +0900

    부모 노드 찾기 (BFS)

--- Backjoon/src/Main.java
+++ Backjoon/src/Main.java
@@ -1,94 +1,92 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
    public static void main(String[] args) throws IOException {
       
       BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
-      //StringBuilder results = new StringBuilder();
+      StringBuilder results = new StringBuilder();
       
-      String[] inputs = scan.readLine().split(" ");
-      int width = Integer.parseInt(inputs[0]);
-      int height = Integer.parseInt(inputs[1]);
+      int nCount = Integer.parseInt(scan.readLine());
       
-      int[][] box = new int[height][width];
-      for(int i = 0; i < height; i++) {
-         String[] tomatos = scan.readLine().split(" ");
-         for(int j = 0; j < width; j++) {
-            box[i][j] = Integer.parseInt(tomatos[j]);
-         }
+      Node[] nodes = new Node[nCount+1];
+      for(int i = 1; i < nCount+1; i++) {
+    	  nodes[i] = new Node(i);
       }
       
-      int result = getMinimumDaysForTomatos(box, width, height);
+      for(int i = 0; i < nCount-1; i++) {
+    	  String[] parsedStr = scan.readLine().split(" ");
+    	  int num1 = Integer.parseInt(parsedStr[0]);
+    	  int num2 = Integer.parseInt(parsedStr[1]);
+    	  
+    	  nodes[num1].connect(nodes[num2]);
+    	  nodes[num2].connect(nodes[num1]);
+      }
       
-      if(!isRipenedAll(box)) result = -1;
+      int[] parentOfNode = new int[nCount+1];
+      boolean[] isVisited = new boolean[nCount+1];
       
-      System.out.print(result);
+      // root node부터 찾기 시작
+      findAllParentOfNode(parentOfNode, isVisited, nodes[1]); 
+      
+      for(int i = 2; i < nCount+1; i++) {
+    	  results.append(parentOfNode[i] + "\n");
+      }
+      
+      System.out.print(results);
       
       scan.close();
    }
    
-   private static int getMinimumDaysForTomatos(int[][] box, int width, int height) {
-      
-	   LinkedList<Position> queue = new LinkedList<>();
-	   for(int i = 0; i < height; i++) {
-		   for(int j = 0; j < width; j++) {
-			   if(box[i][j] == 1) {
-				   queue.add(new Position(i, j));
-			   }
-		   }
-	   }
+   private static void findAllParentOfNode(int[] parentOfNode, boolean[] isVisited, Node root) {
 	   
-	   int day = -1;
+	   LinkedList<Node> queue = new LinkedList<>();
+	   isVisited[root.num] = true;
+	   queue.add(root);
+	   
 	   while(!queue.isEmpty()) {
-		   day++;
 		   int size = queue.size();
 		   for(int i = 0; i < size; i++) {
-			   Position pos = queue.pop();
-			   int x = pos.x;
-			   int y = pos.y;
-			   
-			   if(x > 0 && box[x-1][y] == 0) { // 위쪽
-				   box[x-1][y] = 1;
-				   queue.add(new Position(x-1, y));
-			   }
-			   if(x < height-1 && box[x+1][y] == 0) { // 아래쪽
-				   box[x+1][y] = 1;
-				   queue.add(new Position(x+1, y));
-			   }
-			   if(y > 0 && box[x][y-1] == 0) { // 왼쪽
-				   box[x][y-1] = 1;
-				   queue.add(new Position(x, y-1));
-			   }
-			   if(y < width-1 && box[x][y+1] == 0) { // 오른쪽
-				   box[x][y+1] = 1;
-				   queue.add(new Position(x, y+1));
+			   Node parent = queue.pop();   
+			   for(int j = 0; j < parent.connects.size(); j++) {
+				   Node child = parent.connects.get(j);
+				   if(!isVisited[child.num]) {
+					   isVisited[child.num] = true;
+					   parentOfNode[child.num] = parent.num; 
+					   queue.add(child);
+				   }
 			   }
 		   }
 	   }
-	   
-	   return day < 0 ? 0 : day;
-   }
-   
-   private static boolean isRipenedAll(int[][] box) {
-	   for(int i = 0; i < box.length; i++) {
-		   for(int j = 0; j < box[i].length; j++) {
-			   if(box[i][j] == 0) return false;
-		   }
-	   }
-	   return true;
    }
 }
 
-class Position {
-	int x;
-	int y;
+class Node {
+	List<Node> connects;
+	int num;
 	
-	public Position(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Node(int n) {
+		this.connects = new ArrayList<>();
+		this.num = n;
+	}
+	
+	public void connect(Node other) {
+		connects.add(other);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Node " + num + " : ");
+		for(int i = 0; i < connects.size(); i++) {
+			sb.append(connects.get(i).num + ", ");
+		}
+		
+		return sb.toString();
 	}
 }

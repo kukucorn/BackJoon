748227cad43128114c309cae3b3216b9b2c7a018
jungheun9 <jungheun99@naver.com>
2020-12-11 22:16:55 +0900

    BinarySearch 나무 자르기(백준 2805번)

---
+++ Backjoon/src/BinarySearch/CuttingTree.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CuttingTree {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int numOfTree = Integer.parseInt(st.nextToken());
+		int lengthOfNeed = Integer.parseInt(st.nextToken());
+		
+		int[] treeArr = new int[numOfTree];
+		int max = initArr(treeArr, new StringTokenizer(br.readLine()));
+		
+		long left = 0;
+		long right = max;
+		
+		while(left <= right) {
+			long mid = (left + right) / 2;
+			long sum = 0;
+			for(int i = 0; i < numOfTree; i++) {
+				long cut = treeArr[i] - mid;
+				if(cut > 0) {
+					sum += cut;
+				}
+			}
+			
+			if(sum == lengthOfNeed) {
+				right = mid;
+				break;
+			} else if(sum > lengthOfNeed) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		
+		System.out.print(right);
+		
+		br.close();
+	}
+	
+	private static int initArr(int[] arr, StringTokenizer st) {
+		int max = 0;
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+}
